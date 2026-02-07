711e207f8b004afe998e3bf658b4683b1f270acd
Hana <hanabeimail@gmail.com>
2026-02-07 16:15:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "thank you THANK YOUUUUUUUUUUUUUU MY LOVE",
+        message: "i love you so much baby, i cant wait to spend eternity with you :)",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
